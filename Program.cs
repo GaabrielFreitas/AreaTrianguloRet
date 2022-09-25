bb2a4601911b750980785636869470d52e29fa77
unknown <freitascontato2004@gmail.com>
2022-09-25 16:49:57 -0300

    Resolução do exercício AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double Base, Altura, Area;
+
+Console.Write("Base....:  ");
+Base = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Altura..:  ");
+Altura = Convert.ToDouble(Console.ReadLine());
+
+Area = (Base * Altura) / 2;
+
+Console.WriteLine($"Área..: {Area:N3}");
